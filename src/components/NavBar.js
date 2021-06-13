71907d36726073bde76c656bac1e053e520a8700
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-06-13 14:57:14 -0400

    multiple context

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,23 @@ import React, { Component } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
 export default class NavBar extends Component {
-    static contextType = ThemeContext
     render() {
-        const {isLightTheme, light, dark} = this.context;
-        const theme = isLightTheme ? light : dark;
         return (
-            <nav style={{background:theme.ui, color:theme.syntax}}>
-            <h1>Context APP</h1>
-            <ul>
-                <li>Home</li>
-                <li> About</li>
-                <li> Contact</li>
-
-            </ul>
-        </nav>
+            <ThemeContext.Consumer>{(context) => {
+                const { isLightTheme, light, dark } = context;
+                const theme = isLightTheme ? light : dark;
+        
+                return (
+                    <nav style={{ background: theme.ui, color: theme.syntax }}>
+                    <h1>Context APP</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li> About</li>
+                        <li> Contact</li>
+                    </ul>
+                </nav>
+                )
+            }}</ThemeContext.Consumer>
         )
     }
 }
-
-
-
-
