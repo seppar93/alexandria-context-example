971cc3113bd91bb0a264e1ae791fab72a5bb22dc
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-06-13 15:57:27 -0400

    useContext

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
 
-export default class BookList extends Component {
-    static contextType = ThemeContext
+// export default class BookList extends Component {
+//     static contextType = ThemeContext
 
-    render() {
-        const {isLightTheme, light, dark} = this.context;
-        const theme = isLightTheme ? light : dark;
+//     render() {
+        // const {isLightTheme, light, dark} = this.context;
+        // const theme = isLightTheme ? light : dark;
 
-        return (
-            <div className='book-list' style={{color:theme.syntax, background:theme.bg}}>
+//         return (
+//             <div className='book-list' style={{color:theme.syntax, background:theme.bg}}>
+//             <ul>
+//                 <li style={{background:theme.ui}}>Books</li>
+//                 <li style={{background:theme.ui}}>Books</li>
+//                 <li style={{background:theme.ui}}>Books</li>
+//                 <li style={{background:theme.ui}}>Books</li>
+//             </ul>
+//         </div>
+//         )
+//     }
+// }
+
+const BookList = () => {
+    const {isLightTheme, light, dark } = useContext(ThemeContext)
+    const theme = isLightTheme ? light : dark;
+
+    return ( 
+        <div className='book-list' style={{color:theme.syntax, background:theme.bg}}>
             <ul>
                 <li style={{background:theme.ui}}>Books</li>
                 <li style={{background:theme.ui}}>Books</li>
@@ -18,8 +35,9 @@ export default class BookList extends Component {
                 <li style={{background:theme.ui}}>Books</li>
             </ul>
         </div>
-        )
-    }
+     );
 }
+ 
+export default BookList;
 
 
