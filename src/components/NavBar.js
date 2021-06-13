dea6db6da0467ab690be2d4fa44a5c233b327a1d
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-06-13 15:10:34 -0400

    multiple contexts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react'
+import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext'
 
 export default class NavBar extends Component {
     render() {
         return (
-            <ThemeContext.Consumer>{(context) => {
-                const { isLightTheme, light, dark } = context;
-                const theme = isLightTheme ? light : dark;
-        
-                return (
-                    <nav style={{ background: theme.ui, color: theme.syntax }}>
-                    <h1>Context APP</h1>
-                    <ul>
-                        <li>Home</li>
-                        <li> About</li>
-                        <li> Contact</li>
-                    </ul>
-                </nav>
-                )
-            }}</ThemeContext.Consumer>
+            <AuthContext.Consumer>{(authContext) => (
+                <ThemeContext.Consumer>{(themeContext) => {
+                    const {isAuthenticated, toggleAuth} = authContext
+                    const { isLightTheme, light, dark } = themeContext;
+                    const theme = isLightTheme ? light : dark;
+
+                    return (
+                        <nav style={{ background: theme.ui, color: theme.syntax }}>
+                            <h1>Context APP</h1>
+                                 <div onClick={toggleAuth}>
+                                     {isAuthenticated ? 'Logged In' : "Logged out"}
+                                 </div>
+                            <ul>
+                                <li>Home</li>
+                                <li> About</li>
+                                <li> Contact</li>
+                            </ul>
+                        </nav>
+                    )
+                }}</ThemeContext.Consumer>
+            )}</AuthContext.Consumer>
         )
     }
 }
