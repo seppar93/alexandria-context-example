79196312d975afca9bbcb7705bf88ea598159bf1
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-06-13 13:52:13 -0400

    setup

---
+++ src/components/BookList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BookList = () => {
+    return (
+        <div className='book-list'>
+            <ul>
+                <li>Books</li>
+                <li>Books</li>
+                <li>Books</li>
+                <li>Books</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BookList
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Context APP</h1>
+            <ul>
+                <li>Home</li>
+                <li> About</li>
+                <li> Contact</li>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
+
+
+
