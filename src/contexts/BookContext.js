da433d0f58556292bc8474118f1b6250e0256c23
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-06-16 17:39:07 -0400

    usereducer

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,25 +1,24 @@
-import React, {createContext, useState} from 'react'
+import React, {createContext, useReducer} from 'react'
+import { bookReducer } from '../reducers/bookReducer'
 
 export const BookContext = createContext()
 
 
 
 const BookContextProvider = (props) => {
-  const [books, setBooks] =  useState([
-        {title: 'Name', author: 'some guy', id: 1},
-        {title: 'Name', author: 'some guy', id: 2},
-        {title: 'Name', author: 'some guy', id: 3},
-        {title: 'Name', author: 'some guy', id: 4},
-        {title: 'Name', author: 'some guy', id: 5}
+  const [books, dispatch] =  useReducer( bookReducer, [
     ])
-    const addBook = (title, author) => {
-        setBooks([...books, {title, author, id: Math.random()}])
-    }
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id))
-    }
+    // const addBook = (title, author) => {
+    //     setBooks([...books, {title, author, id: Math.random()}])
+    // }
+    // const removeBook = (id) => {
+    //     setBooks(books.filter(book => book.id !== id))
+    // }
     return (
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        // <BookContext.Provider value={{books, addBook, removeBook}}>
+
+        <BookContext.Provider value={{books, dispatch}}>
+
             {props.children}
         </BookContext.Provider>
     )
